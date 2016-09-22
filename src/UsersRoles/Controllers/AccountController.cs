3bad46f5652badded601d1874b574ffac3f9eb99
Shradha Pulla <pullashradha@gmail.com>
2016-09-22 15:21:32 -0700

    Add Account folder & AccountController to separate user register & login functions from Home

---
+++ src/UsersRoles/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersRoles.Models;
+using UsersRoles.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace UsersRoles.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Register New Account
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var newUser = new User { FirstName = model.FirstName, LastName = model.LastName, UserName = model.Username };
+            IdentityResult registeredUser = await _userManager.CreateAsync(newUser, model.Password);
+            if (registeredUser.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //Log In User
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //Log Out User
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- src/UsersRoles/Controllers/HomeController.cs
+++ src/UsersRoles/Controllers/HomeController.cs
@@ -12,68 +12,15 @@ namespace UsersRoles.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext db)
+        public HomeController(ApplicationDbContext db)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
             _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        //Register New Account
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            var newUser = new User { FirstName = model.FirstName, LastName = model.LastName, UserName = model.Username };
-            IdentityResult registeredUser = await _userManager.CreateAsync(newUser, model.Password);
-            if (registeredUser.Succeeded)
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        //Log In User
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent: true, lockoutOnFailure: false);
-            if (signInResult.Succeeded)
-            {
-                return RedirectToAction("Index", "User");
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        //Log Out User
-        [HttpPost]
-        public async Task<IActionResult> LogOut()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index");
         }
     }
 }
