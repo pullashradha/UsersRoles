c30cc6e98758e53f46b208e48fae93c13ce3d723
Shradha Pulla <pullashradha@gmail.com>
2016-09-22 13:41:18 -0700

    Migrate databases, add register user functionality(RegisterViewModel, Register view, Register() method in HomeController)

--- src/UsersRoles/Controllers/HomeController.cs
+++ src/UsersRoles/Controllers/HomeController.cs
@@ -11,9 +11,39 @@ namespace UsersRoles.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Register New Account
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var newUser = new User { UserName = model.Username };
+            IdentityResult registeredUser = await _userManager.CreateAsync(newUser, model.Password);
+            if (registeredUser.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- src/UsersRoles/Models/ApplicationDbContext.cs
+++ src/UsersRoles/Models/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace UsersRoles.Models
 {
