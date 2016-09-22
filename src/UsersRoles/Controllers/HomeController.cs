0f3691d0c1047d9a50b244024b25d6934f1010df
Shradha Pulla <pullashradha@gmail.com>
2016-09-22 13:30:06 -0700

    Create basic files & folders

---
+++ src/UsersRoles/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersRoles.Models;
+using UsersRoles.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersRoles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
